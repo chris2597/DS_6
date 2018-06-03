446f17f92ae1e4feba20adf9b56d0740580a5ece
Christopher Jiménez <christopher.jimenez@utp.ac.pa>
2018-06-03 01:27:00 -0600

    up archive

---
+++ app/src/main/java/com/example/christopher/parcial1/SplashActivity.java
@@ -0,0 +1,22 @@
+package com.example.christopher.parcial1;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Handler _handler = new Handler();
+        _handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent _intent = new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(_intent);
+                finish();
+            }
+        },3000);
+    }
+}
